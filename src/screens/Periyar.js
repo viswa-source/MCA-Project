f10e4767db205ee5e63244f482c32b03f49a95a1
viswa <viswasekaran@olivecrypto.com>
2022-02-20 18:52:08 +0530

    Mobile view

--- src/screens/Periyar.js
+++ src/screens/Periyar.js
@@ -3,17 +3,15 @@ import "./Periyar.css";
 export default function Periyar() {
   return (
     <div className="container">
-      <div className="text_container">
-    
-      <p  className="text_periyar">
-        There is no god, there is no god, there is no god at all. He who
-        invented god is a fool. He who propagates god is a scoundrel. He who
-        worships god is a barbarian.s
-      </p>
-      
+      <div className="text_container col-2">
+        <p className="text_periyar lead">
+          There is no god, there is no god, there is no god at all. He who
+          invented god is a fool. He who propagates god is a scoundrel. He who
+          worships god is a barbarian.s
+        </p>
       </div>
       <img
-        className="img_periyar"
+        className="img_periyar img-fluid col-10"
         style={{ alignSelf: "flex-start" }}
         src={require("../images/periyar.png")}
       />

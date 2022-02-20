7d81f5cbca3da2494ec0a26b014da7d713234b57
kannan0929 <kannanak0929@gmail.com>
2022-02-20 19:06:40 +0530

    Message

--- src/screens/Journalism.js
+++ src/screens/Journalism.js
@@ -1,7 +1,22 @@
-import React from 'react'
+import React from "react";
 
 export default function Journalism() {
   return (
-    <div>Journalism</div>
-  )
+    <div className="container">
+      <div className="text_container">
+        <p className="text_periyar">
+          Journalism is the production and distribution of reports on the
+          interaction of events, facts, ideas, and people that are the "news of
+          the day" and that informs society to at least some degree. ...
+          Journalistic media include: print, television, radio, Internet, and,
+          in the past, newsreels
+        </p>
+      </div>
+      <img
+        className="img_periyar"
+        style={{ alignSelf: "flex-start" }}
+        src={require("../images/journalism.png")}
+      />
+    </div>
+  );
 }
